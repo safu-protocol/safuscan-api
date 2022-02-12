bf37b98addbdfdc0e451f326073acbb9d099517d
gregmeszaros <meszaros275@gmail.com>
2022-02-12 22:01:58 +0000

    Add TODO

--- src/routes/info.ts
+++ src/routes/info.ts
@@ -4,12 +4,15 @@ import { Token } from '../../models/token'
 const router = express.Router()
 
 router.get('/api/info', async (req: Request, res: Response) => {
+    // 1.1
     // @TODO -> pass token contract address and chain ID (BSC, ETH, Polygon)
-    const tokenInfo = await Token.find({})
+    const tokenInfo = await Token.find({}) // If no results go to 1.2
     return res.status(200).send(tokenInfo)
 })
 
 router.post('/api/info', async (req: Request, res: Response) => {
+    // 1.2
+    // @TODO -> if the token info doesn't exist, query Moralis API, BSCscan and save the required data!
     const { total_supply, burned_tokens } = req.body;
 
     const tokenAdd = Token.build({ total_supply, burned_tokens })
