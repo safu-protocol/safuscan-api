18bf64a871601bfa4659cc27e1b5c266ddf9c481
mark0w <mark0w@users.noreply.github.com>
2022-04-14 02:25:33 +0300

    Remove Moralis API calls

--- services/bsc-scan.service.ts
+++ services/bsc-scan.service.ts
@@ -1,25 +1,22 @@
 import fetch from 'node-fetch';
+import { BscScanData } from '../models/bsc-scan.response'
 
-export async function getTokenTotalSupply(contractAddress: string) {
+export async function getTokenTotalSupply(contractAddress: string): Promise<BscScanData> {
     const url = "https://api.bscscan.com/api" +
     "?module=stats" +
     "&action=tokensupply" +
     `&contractaddress=${contractAddress}` +
     `&apikey=${process.env.BSC_API_KEY}`
 
-    return await fetch(url).then((response: any) => {
-        return response.json ? response.json() : response.text();
-    });
+    return await fetch(url).then(res => res.json()).then(json => new BscScanData(json))
 };
 
-export async function getCirculatingSupply(contractAddress: string) {
+export async function getCirculatingSupply(contractAddress: string): Promise<BscScanData> {
     const url = "https://api.bscscan.com/api" +
     "?module=stats" +
     "&action=tokenCsupply" +
     `&contractaddress=${contractAddress}` +
     `&apikey=${process.env.BSC_API_KEY}`
 
-    return await fetch(url).then((response: any) => {
-        return response.json ? response.json() : response.text();
-    });
+    return await fetch(url).then(res => res.json()).then(json => new BscScanData(json))
 }
